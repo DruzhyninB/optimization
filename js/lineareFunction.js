94b60de36c087298998e56f9fc602dec47067a56
DinamickaDevelopment <info@dinamicka.com>
2015-12-30 05:16:50 +0200

    finish

--- js/lineareFunction.js
+++ js/lineareFunction.js
@@ -1,35 +1,60 @@
 ﻿$(document).ready(function () {
     $('#go').click(takeProp)
+    function Error(a) {
+        $('.errorPop').html(a);
+        $('.errorPop').show();
+        setTimeout(function () {
+            $('.errorPop').hide();
+            $('.errorPop').html('');
+        }, 5000)
+    }
+    function write(str) {
+        $('#result').html($('#result').html() + str + '<br/>');
+    }
     function takeProp() {
+        if ($('#count').val() == '') {
+            Error('"Max power of func" field is empty, check it again.');
+            return false;
+        }
         count = +$('#count').val();
         for (var i = 1; i < count+1; i++) {
             elem1 = document.createElement('input');
             elem2 = document.createElement('input');
-            //labelelem1 = document.createElement('label');
-            //labelelem2 =document.createElement('label');
             elem1.type = elem2.type = 'text';
             elem1.id = 'a' + i;
             elem2.id = 'p' + i;
             elem1.placeholder = 'a' + i;
             elem2.placeholder = 'p' + i;
-            //labelelem1.setAttribute('for', 'a' + i);
-            //labelelem2.setAttribute('for', 'p' + i);
-            //labelelem1.innerHTML = 'a' + i;
-            //labelelem2.innerHTML = 'p' + i;
-            //document.getElementById('console').appendChild(labelelem1);
             document.getElementById('console').appendChild(elem1);
-            //document.getElementById('console').appendChild(labelelem2);
             document.getElementById('console').appendChild(elem2);
         }
+        //Freepart generate
         freepart = document.createElement('input');
         freepart.type = 'text';
         freepart.id = 'c';
         freepart.placeholder = 'c';
         document.getElementById('console').appendChild(freepart);
-
+        //area EndPint input generate
+        areaL = document.createElement('input');
+        areaR = document.createElement('input');
+        areaL.type = areaR.type = 'text';
+        areaL.id = 'arL';
+        areaR.id = 'arR';
+        areaL.placeholder = 'Area A'
+        areaR.placeholder = 'Area B'
+        document.getElementById('console').appendChild(areaL);
+        document.getElementById('console').appendChild(areaR);
+        //Step field generate
+        step = document.createElement('input');
+        step.type = 'text';
+        step.id = 'step';
+        step.placeholder = 'step( h )';
+        document.getElementById('console').appendChild(step);
+        //action btn generate
         btn = document.createElement('button');
         btn.id = 'prop-btn';
         btn.innerHTML = 'Done';
+
         document.getElementById('console').appendChild(btn);
         $('#prop-btn').click(calculate);
 
@@ -37,18 +62,47 @@
             //output of F(x)
             var Fx = 'F(x) = ',F$x='F\'(x) = ';
             for (var i = 1; i < count + 1; i++) {
+                
                 Fx = Fx + $('#a' + i).val() + 'x<sup>' + $('#p' + i).val() + '</sup> + ';
             }
             Fx = Fx + $('#c').val();
-            $('#result').html(Fx + '<br/>');
-            //output of f'(x);
-            for (var i = 1; i < count+1; i++) {
-                m=$('#a' + i).val() *$('#p' + i).val();
-                k=$('#p' + i).val() - 1;
-                F$x = F$x + m + 'x<sup>' + k + '</sup> + '
+            write(Fx);
+            write('Have segment [ ' + $('#arL').val() + ' , ' + $('#arR').val() + ' ] and Step = ' + $("#step").val());
+            //take number of iteration
+            Niteration = (($('#arR').val() - $('#arL').val()) / $("#step").val())+1;
+            write('Number of iteration : ' + Niteration)
+            //Descripteion on process
+            var dots = [+$('#arL').val()], step = +$("#step").val(), str = 'X<sub>k</sub> =',temp;
+            for (var i = 0; i < Niteration; i++) {
+                temp = dots[i] + step;
+                dots.push(+temp.toFixed(10));//because of specific calculation of JavaScript;
+                str += ' ' + dots[i]+',';
             }
-            document.getElementById('result').innerHTML += F$x;
-
+            write(str);
+            write('<hr>')
+            //begin of algoritm
+            var Fopt = fx(dots[0]),xopt = dots[0],Fwork;
+            for (var i = 0; i < Niteration; i++) {
+                write('<span class="step">Step<span> ' + (i + 1));
+                Fwork = fx(dots[i]);
+                write('F(' + dots[i] + ') = ' + Fwork);
+                if (Fwork < Fopt) {
+                    Fopt = Fwork;
+                    xopt = dots[i];
+                }
+            }
+            write('Our min value of funcion - F('+xopt+') = '+Fopt)
+        }
+        function fx(k) {
+            var fvalue = 0, a, n,
+                c = ($('#c').val() !== "") ? +$('#c').val() : 0;//if input C empty -> c=0, else get input value
+            for (var j = 1; j < count + 1; j++) {
+                a = +$('#a'+j).val();
+                n = +$('#p'+j).val();
+                fvalue += a * Math.pow(k, n);
+            }
+            fvalue = fvalue + c;
+            return fvalue;
         }
     }
 });
--- js/pohidna.js
+++ js/pohidna.js
@@ -2,17 +2,18 @@
     $('#go').click(derivate);
 
     function derivate() {
-        var e = $('#acc').val(), dx = 0.1, x = 1, i = 0;
+        var e = $('#acc').val(), dx = 0.1, x = 1, i = 1;
         e = +e;
         f1 = 0;
         do{
             f2=f1;
             f1 = (mathFunction(x + dx) - mathFunction(x)) / dx;
-            $('#console').html($('#console').html() + 'F' + '<sup>' + i + '</sup>' + ' = ' + f1 + '<br/>');
+            $('#console').html($('#console').html() + 'F' + '<sup>' + i + '</sup>' + ' = ' + f1.toFixed(4) + '<br/>');
             i++;
-            dx = dx / 2;
+            dx = dx / i;
             a = Math.abs(f1 - f2);
         } while (Math.abs(f1 - f2) >= e);
+        return f1;
     }
     function mathFunction(a) {
         return a * a;
